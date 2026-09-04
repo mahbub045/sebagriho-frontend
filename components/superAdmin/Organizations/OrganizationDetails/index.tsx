c3be749678986de1ec031167b0de8b2d4cf59b7f
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2026-09-04 11:51:11 +0600

    ogr slug added

--- components/superAdmin/Organizations/OrganizationDetails/index.tsx
+++ components/superAdmin/Organizations/OrganizationDetails/index.tsx
@@ -1,12 +1,17 @@
 import Breadcrumbs from '@/components/common/Breadcrumbs/Breadcrumbs';
+import { useParams } from 'next/navigation';
 
 const OrganizationDetailsContainer: React.FC = () => {
+  const { organizationslug } = useParams();
   return (
     <div>
       <Breadcrumbs
         items={[
-          { label: 'Organizations', href: '/organizations' },
-          { label: 'Organization Details', href: '/organizations/details' },
+          { label: 'Organizations', href: `/super-admin/organizations` },
+          {
+            label: 'Organization Details',
+            href: `/super-admin/organizations/${organizationslug}`,
+          },
         ]}
       />
     </div>
--- components/superAdmin/Organizations/index.tsx
+++ components/superAdmin/Organizations/index.tsx
@@ -4,11 +4,8 @@ import OrganizationList from './OrganizationList/OrganizationList';
 const OrganizationsContainer: React.FC = () => {
   return (
     <div>
-     <Breadcrumbs
-        items={[
-          { label: 'Organizations', href: '/organizations' },
-          { label: 'Organization Details', href: '/organizations/details' },
-        ]}
+      <Breadcrumbs
+        items={[{ label: 'Organizations', href: `/super-admin/organizations` }]}
       />
       <OrganizationList />
     </div>
