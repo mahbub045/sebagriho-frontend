28e82aec9dafed54b020fe1e830dfcb7d5beeaff
Md Mahbub Rahman <mahbubrmn45@gmail.com>
2026-09-04 18:00:29 +0600

    Add button

--- components/superAdmin/Organizations/index.tsx
+++ components/superAdmin/Organizations/index.tsx
@@ -1,4 +1,6 @@
 import Breadcrumbs from '@/components/common/Breadcrumbs/Breadcrumbs';
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 import OrganizationCards from './OrganizationCards/OrganizationCards';
 
 const OrganizationsContainer: React.FC = () => {
@@ -8,9 +10,19 @@ const OrganizationsContainer: React.FC = () => {
         items={[{ label: 'Organizations', href: `/super-admin/organizations` }]}
       />
 
-      <div>
-        <h1 className="text-2xl font-bold">Organizations</h1>
-        <p className="text-muted-foreground">Welcome to the Organizations page</p>
+      <div className='mb-4 flex items-center justify-between'>
+        <div>
+          <h1 className='text-2xl font-bold'>Organizations</h1>
+          <p className='text-muted-foreground'>
+            Welcome to the Organizations page
+          </p>
+        </div>
+        <div>
+          <Button>
+            <Plus />
+            Add Organization
+          </Button>
+        </div>
       </div>
 
       <OrganizationCards />
